e478a7796e5a9d9e935d05b121eca867f33400e0
Fadad Oussama <fadadoussama7@gmail.com>
2023-06-03 19:38:40 +0200

    switch useState to useReducer

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -4,12 +4,8 @@ import { cartContext } from "../context/cart.context";
 import { productContext } from "../context/product.context";
 
 export default function CartIcon() {
-  const { isCartOpen, setIsCartOpen } = useContext(cartContext);
+  const { handleCartDropdown } = useContext(cartContext);
   const { productCount } = useContext(productContext);
-
-  const handleCartDropdown = () => {
-    setIsCartOpen(!isCartOpen);
-  };
 
   return (
     <div className="w-[40px] h-[40px] relative ml-5 cursor-pointer" onClick={handleCartDropdown}>
--- src/components/context/cart.context.jsx
+++ src/components/context/cart.context.jsx
@@ -1,13 +1,30 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useReducer } from "react";
 import { Outlet } from "react-router-dom";
+
+const reducerFun = (isCartOpen, action) => {
+  const { type } = action;
+
+  switch (type) {
+    case "click":
+      return !isCartOpen;
+    default:
+      throw new Error(`unhandled type ${type} in reducer function`);
+  }
+};
 
 export const cartContext = createContext();
 
 export default function CartProvider() {
-  const [isCartOpen, setIsCartOpen] = useState(false);
+  const [isCartOpen, dispatch] = useReducer(reducerFun, false);
+
+  const handleCartDropdown = () => {
+    dispatch({
+      type: "click",
+    });
+  };
 
   return (
-    <cartContext.Provider value={{ isCartOpen, setIsCartOpen }}>
+    <cartContext.Provider value={{ isCartOpen, handleCartDropdown }}>
       <Outlet />
     </cartContext.Provider>
   );
